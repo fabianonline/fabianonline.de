315ef467bcc3686f5ed39284d2251892288e7320
Fabian Schlenz <mail@fabianonline.de>
2011-05-29 23:22:29 +0200

    Bilder vom Serendipity-Blog herüberkopiert. Dazu: Bilder-Link-Anpass-Skript erweitert und damit die Posts aus dem Serendipity-Blog angepasst.

--- _scripts/modify_image_links.sh
+++ _scripts/modify_image_links.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-ruby -pi.bak1 -e "gsub(/http:\/\/blog.fabianonline.de\/wp-content\/main\//, '/uploads/images/')" ../_posts/*.markdown
-ruby -pi.bak2 -e "gsub(/http:\/\/blog.fabianonline.de\/wp-content\/uploads\//, '/uploads/images/')" ../_posts/*.markdown
+#ruby -pi.bak1 -e "gsub(/http:\/\/blog.fabianonline.de\/wp-content\/main\//, '/uploads/images/')" ../_posts/*.markdown
+#ruby -pi.bak2 -e "gsub(/http:\/\/blog.fabianonline.de\/wp-content\/uploads\//, '/uploads/images/')" ../_posts/*.markdown
+
+#ruby -pi.bak3 -e "gsub(/\"\/blog\/uploads\//, '\"/uploads/images/serendipity/')" ../_posts/*.html
+ruby -pi.bak4 -e "gsub(/'\/blog\/uploads\//, '\'/uploads/images/serendipity/')" ../_posts/*.html
+
 
